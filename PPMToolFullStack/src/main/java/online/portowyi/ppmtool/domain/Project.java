49b1dbe09eeb1041b8ab3e7c9bb83322b1e5ad22
PORTwein <port1986.86@mail.ru>
2020-10-31 02:02:16 +0300

    Customise Project object Add lombok in dependencies Add repository ProjectRepository

--- PPMToolFullStack/src/main/java/online/portowyi/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/online/portowyi/ppmtool/domain/Project.java
@@ -1,4 +1,34 @@
 package online.portowyi.ppmtool.domain;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity @Getter @Setter @NoArgsConstructor
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+    private Date created_at;
+    private Date updated_at;
+
+    @PrePersist
+    protected void prePersist(){
+        this.created_at = new Date();
+    }
+
+    @PreUpdate
+    protected void preUpdate(){
+        this.updated_at = new Date();
+    }
 }
