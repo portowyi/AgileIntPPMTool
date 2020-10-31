9f0c116f1bee3a9688ad9fe010ba07e50c8649a9
PORTwein <port1986.86@mail.ru>
2020-10-31 03:24:40 +0300

    Продолжаю издеваться над валидацией ()

--- PPMToolFullStack/src/main/java/online/portowyi/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/online/portowyi/ppmtool/web/ProjectController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
@@ -22,7 +24,7 @@ public class ProjectController {
     }
 
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project){
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
