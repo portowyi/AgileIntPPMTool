5316994cf3a081dc8b9c56281fb63cef1f3dd7e8
DESKTOP-P8B8UKM\port1 <port1986.86@gmail.com>
2020-11-07 22:23:32 +0300

    bootstrap Добавлен

--- ppm-tool-project/src/components/Dashboard.js
+++ ppm-tool-project/src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import Header from './Layout/Header'
-import ProjectItem from './Project/ProjectItem'
+import ProjectItem from './Project/ProjectItem';
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to our APP</h1>
+                <h1 className="alert alert-warning">Welcome to our APP</h1>
                 <Header />
                 <ProjectItem />
             </div> 
