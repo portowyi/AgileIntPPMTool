197258ad6d6c372492c915045facfb53a9292ec8
DESKTOP-P8B8UKM\port1 <port1986.86@gmail.com>
2020-11-08 11:12:20 +0300

    123456

--- ppm-tool-project/src/App.js
+++ ppm-tool-project/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Dashboard />
+        <Route path="/addProject" component={AddProject}/>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+ 
---
+++ ppm-tool-project/src/components/Project/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddProject extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Форма создания нового проекта</h1>
+            </div>
+        )
+    }
+}
+
+export default AddProject; 
